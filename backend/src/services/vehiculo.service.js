62321021ee08595dbe3a75720f3b8467f362ea21
John-Henriquez <hjohnalexander@gmail.com>
2025-01-14 19:36:50 -0300

    add tipo_vehiculo

--- backend/src/services/vehiculo.service.js
+++ backend/src/services/vehiculo.service.js
@@ -2,12 +2,26 @@
 
 import { AppDataSource } from "../config/configDb.js";
 import  Vehiculo  from "../entity/vehiculo.entity.js";
+import TipoVehiculo from "../entity/tipo_vehiculo.entity.js";
 
 export async function createVehiculoService(vehiculoData) {
   try {
+    const { id_tipo_vehiculo, ...restoVehiculo } = vehiculoData;
+
+    const tipoRepository = AppDataSource.getRepository(TipoVehiculo);
+    const tipoVehiculo = await tipoRepository.findOne({ where: { id: id_tipo_vehiculo } });
+
+    if (!tipoVehiculo) {
+      return [null, "El tipo de vehículo no existe"];
+    }
+
     const vehiculoRepository = AppDataSource.getRepository(Vehiculo);
     
-    const vehiculo = vehiculoRepository.create(vehiculoData);
+    const vehiculo = vehiculoRepository.create({
+      ...restoVehiculo,
+      tipo: tipoVehiculo, 
+    });
+
     await vehiculoRepository.save(vehiculo);
 
     return [vehiculo, null];
@@ -16,6 +30,7 @@ export async function createVehiculoService(vehiculoData) {
     return [null, "Error interno del servidor"];
   }
 }
+
 
 export async function getAllVehiculosService() {
   try {
@@ -81,3 +96,16 @@ export async function deleteVehiculoService(placa) {
     return [null, "Error interno del servidor"];
   }
 }
+
+
+export async function getAllTiposVehiculoService() {
+  try {
+    const tipoRepository = AppDataSource.getRepository(TipoVehiculo);
+    const tipos = await tipoRepository.find();
+    return [tipos, null];
+  } catch (error) {
+    console.error("Error al obtener los tipos de vehículo:", error);
+    return [null, "Error interno del servidor"];
+  }
+}
+
