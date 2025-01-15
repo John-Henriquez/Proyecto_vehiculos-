1a4efc112f9164a2e6667f7fd4fef9cf88a88faa
John_Henriquez <hjohnalexander@gmail.com>
2025-01-15 11:12:04 -0300

    manejo de usuarios

--- backend/src/routes/solicitud.routes.js
+++ backend/src/routes/solicitud.routes.js
@@ -1,5 +1,7 @@
 "use strict";
 import { Router } from "express";
+import { isAdmin } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 import {
   createSolicitud,
   deleteSolicitud,
@@ -10,6 +12,9 @@ import {
 
 const router = Router();
 
+router
+  .use(authenticateJwt)
+  .use(isAdmin);
   
 router
   .get("/", getAllSolicitudes)
